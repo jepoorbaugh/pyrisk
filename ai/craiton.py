b4c6a8e208b29638b415d06cbbeda36096a40099
jepoorbaugh <jepoorbaugh@gmail.com>
2025-04-09 13:06:05 -0400

    Created file

---
+++ ai/craiton.py
@@ -0,0 +1,21 @@
+from ai import AI
+import random
+import collections
+
+
+class CrAItonAI(AI):
+    """
+    CrAItonAI: Uses MCTS to get the best possible next move.
+    """
+
+    def start(self):
+        pass
+
+    def reinforce(self, available):
+        pass
+
+    def attack(self):
+        pass
+
+    def freemove(self):
+        pass
