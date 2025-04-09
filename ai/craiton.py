4835a937a3fe586c6a03a7a2dce4696929a07448
Peyton3995 <143836277+Peyton3995@users.noreply.github.com>
2025-04-09 13:49:26 -0400

    steal swedish guy's code (#13)
    There are people in their mom's maintaining this repo and I am stealing it so I can pass this class. God bless open source and God bless our Devs.

--- ai/craiton.py
+++ ai/craiton.py
@@ -9,7 +9,22 @@ class CrAItonAI(AI):
     """
 
     def start(self):
-        pass
+        self.area_priority = list(self.world.areas)
+        random.shuffle(self.area_priority)
+
+    def priority(self):
+        priority = sorted([t for t in self.player.territories if t.border], 
+                          key=lambda x: self.area_priority.index(x.area.name))
+        priority = [t for t in priority if t.area == priority[0].area]
+        return priority if priority else list(self.player.territories)
+            
+
+    def initial_placement(self, empty, available):
+        if empty:
+            empty = sorted(empty, key=lambda x: self.area_priority.index(x.area.name))
+            return empty[0]
+        else:
+            return random.choice(self.priority())
 
     def reinforce(self, available):
         pass
@@ -18,4 +33,10 @@ class CrAItonAI(AI):
         pass
 
     def freemove(self):
-        pass
+        srcs = sorted([t for t in self.player.territories if not t.border], 
+                      key=lambda x: x.forces)
+        if srcs:
+            src = srcs[-1]
+            n = src.forces - 1
+            return (src, self.priority()[0], n)
+        return None
